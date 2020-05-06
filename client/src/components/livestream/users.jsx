8a6d71109cb4c7786793cc901046bc544ecc32b5
nendale <nahumtefera@gmail.com>
2020-05-05 18:42:23 -0700

    added code to remove camera icon from my username on Users Component

--- client/src/components/livestream/users.jsx
+++ client/src/components/livestream/users.jsx
@@ -6,20 +6,23 @@ import {socket} from '../socketComp/socketComp'
 class Users extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
+      mySocketId: '',
       users: []
     }
   }
 
   componentDidMount() {
     // Get user's data
+    // console.log('my socket id: ', socket.id)
     socket.on('get users', data => {
-      this.setState({users: data})
+      this.setState({users: data, mySocketId: socket.id})
     });
   }
 
   render() { 
     let users = this.state.users;
+    let mySocketId = this.state.mySocketId;
     let createOffer = this.props.createOffer // function to request video call
     return ( 
       <div className="users-container">
@@ -30,9 +33,13 @@ class Users extends Component {
               return (
                 <li key={index} className='username-item' id={user.id}>
                   {user.username}
-                  <div className='camera-icon'>
-                    <img onClick={()=>{createOffer(user.id)}} src={require('./videosvg.svg')} alt="video-icon"/>
-                  </div>
+                  {
+                    user.id === mySocketId
+                      ? null
+                      : <div className='camera-icon'>
+                          <img onClick={()=>{createOffer(user.id)}} src={require('./videosvg.svg')} alt="video-icon"/>
+                        </div>
+                  }
                 </li>
               )
             })
